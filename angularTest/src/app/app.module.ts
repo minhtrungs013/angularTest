3a352d59f63d02e11a5d7d168b19a00af031ae5e
Truong Nguyen <nguyenxuantruong@tma.com.vn>
2023-06-14 14:12:11 +0700

    add

--- angularTest/src/app/app.module.ts
+++ angularTest/src/app/app.module.ts
@@ -1,18 +1,42 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { NzCarouselModule } from 'ng-zorro-antd/carousel';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { NZ_I18N } from 'ng-zorro-antd/i18n';
+import { en_US } from 'ng-zorro-antd/i18n';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { IconsProviderModule } from './icons-provider.module';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';
+import { NzMenuModule } from 'ng-zorro-antd/menu';
+import { HeaderComponent } from './header/header.component';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { SliderComponent } from './slider/slider.component';
+registerLocaleData(en);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    SliderComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    IconsProviderModule,
+    NzLayoutModule,
+    NzCarouselModule,
+    NzMenuModule,
+    NzGridModule,
   ],
-  providers: [],
+  providers: [{ provide: NZ_I18N, useValue: en_US }],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
